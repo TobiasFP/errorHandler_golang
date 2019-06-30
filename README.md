db0985d247cf059eda43e6c678cf790617d2009b
TobiasFP <tobi.fp@gmail.com>
2019-06-30 22:58:51 +0200

    First commit - works

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# errorHandler_golang
-Error handler with mail by mailgun support for golang
+<h1>errorHandler_golang - A simple error handler for go</h1>
+<h4> VERY EXPLICIT Error handler with mail by mailgun support for golang </h4>
+
+<h2> Purpose </h2>
+<p> The purpose of this error handler is, despite it's name, not to handle errors, but rather, notify admins and log, when errors occur. <br />
+The logger is VERY explicit </p>
---
+++ errorhandler.go
@@ -0,0 +1,65 @@
+package errorHandler
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mailgun/mailgun-go"
+)
+
+func IsError(err error, errClass string, mgConf MailgunConf) bool {
+	if err != nil {
+		switch errClass {
+		case "fatal":
+			sendmail(mgConf, "PANIC!!!", "A panicy error occured: "+err.Error())
+			log.Fatal("Panic: ", err, " - ", mgConf.receiver, " has been notified by mail")
+		case "panic":
+			sendmail(mgConf, "PANIC!!!", "A panicy error occured: "+err.Error())
+			log.Panic("Panic: ", err, " - ", mgConf.receiver, " has been notified by mail")
+		case "log&mail":
+			sendmail(mgConf, "A simple error occured", "A simple error occured: "+err.Error()+" - "+mgConf.receiver)
+			log.Print("A simple error occured: ", err, " - ", mgConf.receiver, " has been notified by mail")
+		case "log":
+			log.Print("A simple error occured: ", err)
+		default:
+			sendmail(mgConf, "Poorly set up logger", `Your logger was poorly set up.\n
+			Make sure to choose from the official list of error classes in all functions\n
+			See the github page for a list`)
+			log.Print(`Your logger was poorly set up.\n
+			Make sure to choose from the official list of error classes in all functions\n
+			See the github page for a list`)
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+type MailgunConf struct {
+	mailgunDomain string
+	privateAPIKey string
+	sender        string
+	receiver      string
+}
+
+func sendmail(mgConf MailgunConf, subject string, body string) {
+	// Create an instance of the Mailgun Client
+	mg := mailgun.NewMailgun(mgConf.mailgunDomain, mgConf.privateAPIKey)
+
+	// The message object allows you to add attachments and Bcc recipients
+	message := mg.NewMessage(mgConf.sender, subject, body, mgConf.receiver)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message	with a 10 second timeout
+	resp, id, err := mg.Send(ctx, message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+}
